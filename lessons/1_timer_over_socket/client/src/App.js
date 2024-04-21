eabffdecd6bf50096f69a07ab7517cb6a8652262
Sterren Fisher <13692795+Steztric@users.noreply.github.com>
2024-04-21 10:38:25 +0800

    Created client server

--- lessons/1_timer_over_socket/client/src/App.js
+++ lessons/1_timer_over_socket/client/src/App.js
@@ -1,18 +1,22 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { subscribeToTimer } from './api';
 
-class App extends Component {
+export default function App() {
+  const [timestamp, setTimestamp] = useState('no timestamp yet');
 
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Our awesome drawing app</h2>
-        </div>
-        hello
+  useEffect(() => {
+    subscribeToTimer((timestamp) => {
+      setTimestamp(timestamp);
+    });
+  }, []);
+
+  return (
+    <div className="App">
+      <div className="App-header">
+        <h2>Our awesome drawing app</h2>
       </div>
-    );
-  }
-}
-
-export default App;
+      This is the value of the timer timestamp: {timestamp}
+    </div>
+  );
+};
